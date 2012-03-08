62fc67375e44b41d686e453b745fafe938fdbba1
Jonas Nystrom <jonasnys@gmail.com>
2012-03-08 21:56:57 +0100

    first

---
+++ src/Main.hx
@@ -0,0 +1,19 @@
+package ;
+
+import neko.Lib;
+
+/**
+ * ...
+ * @author Jonas Nyström
+ */
+
+class Main {	
+	static var email = 'jonasnys';
+	static var passwd = '%gloria!';
+	static var keyTestSheet = '0Ar0dMoySp13UdE93Vno1QlJ3cklrLW5zTWItOTRZS2c';
+	
+	static function main() {
+		var gs = new HxGoogleDocs.HxGoogleSpreadsheet(email, passwd, keyTestSheet);
+		trace(gs.getCells());
+	}		
+}
